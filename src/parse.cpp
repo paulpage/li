1a4de1b3abfed70b7176a623d90223db4fa0033b
Paul Page <pjpage98@gmail.com>
2023-01-27 23:17:24 -0600

    Parse all bod/bob files

--- src/parse.cpp
+++ src/parse.cpp
@@ -29,6 +29,11 @@ typedef struct {
     uint32_t name_offset;
 } Entry;
 
+typedef struct {
+    char bod[255];
+    char bob[255];
+} Pack;
+
 uint32_t get_u32(const uint8_t *data, int i) {
     uint32_t val = (data[i])
         + (data[i+1] << 8)
@@ -56,12 +61,15 @@ size_t write_file(const char *filename, uint8_t *data, long len) {
     FILE *f = fopen(filename, "wb");
     if (!f) {
         perror("no file");
+        printf("Failed to write %s\n", filename);
         fflush(0);
         /* printf("no file\n"); */
         return 0;
     }
 
-    return fwrite(data, len, 1, f);
+    size_t result = fwrite(data, len, 1, f);
+    fclose(f);
+    return result;
 }
 
 static void mkdir_recursive(const char *dir) {
@@ -82,7 +90,8 @@ static void mkdir_recursive(const char *dir) {
     mkdir(tmp, S_IRWXU);
 }
 
-char **list_dir(char *dir, int *out_len) {
+Pack *get_pack_list(const char *dir, int *out_len) {
+
     struct dirent *d;
     DIR *dh = opendir(dir);
     if (!dh) {
@@ -90,9 +99,48 @@ char **list_dir(char *dir, int *out_len) {
         return 0;
     }
 
+    Pack *entries = (Pack*)malloc(128 * sizeof(Pack));
+    int count = 0;
     while ((d = readdir(dh)) != NULL) {
+
+
+        Pack entry = {0};
+
+        int c;
+        for (c = 0; dir[c] != '\0'; c++) {
+            entry.bod[c] = dir[c];
+        }
+
+        entry.bod[c] = '/';
+        c++;
+
+        for (int s = 0; d->d_name[s] != '\0'; s++) {
+            entry.bod[c] = d->d_name[s];
+            c++;
+        }
+        entry.bod[c] = '\0';
+
+        if (entry.bod[c - 3] == 'b' && entry.bod[c - 2] == 'o' && entry.bod[c - 1] == 'd') {
+            printf("%s\n", entry.bod);
+
+            memcpy(entry.bob, entry.bod, 255);
+            entry.bob[c - 1] = 'b';
+
+            printf("%s\n", entry.bod);
+            printf("%s\n", entry.bob);
+
+            entries[count] = entry;
+            count++;
+        }
         // TODO
     }
+
+    closedir(dh);
+
+
+    // TODO
+    *out_len = count;
+    return entries;
 }
 
 Entry *extract_entries(const uint8_t *info_data, int offset, int count) {
@@ -155,6 +203,7 @@ void process_entry(uint8_t *info_data, uint8_t *file_data, Entry *entries, int *
             fpos++;
         }
 
+        filename[fpos] = '\0';
         mkdir_recursive(filename);
 
         int j = stack_pos - 1;
@@ -164,17 +213,16 @@ void process_entry(uint8_t *info_data, uint8_t *file_data, Entry *entries, int *
         }
         filename[fpos] = '\0';
 
-        printf("%s\n", filename);
+        /* printf("%s\n", filename); */
         write_file(filename, &file_data[entry.offset], entry.len);
     }
 }
 
-int main(int argc, char **argv) {
-
-    long file_data_len;
+void process_pack(Pack pack) {
     long len;
-    uint8_t *file_data = read_file("/home/paul/Downloads/li2/cabs/Game/_data/z01BPK.bob", &file_data_len);
-    uint8_t *info_data = read_file("/home/paul/Downloads/li2/cabs/Game/_data/z01BPK.bod", &len);
+    long file_data_len;
+    uint8_t *info_data = read_file(pack.bod, &len);
+    uint8_t *file_data = read_file(pack.bob, &file_data_len);
 
     int i;
     i = len - 16;
@@ -185,9 +233,29 @@ int main(int argc, char **argv) {
 
     Entry *entries = extract_entries(info_data, file_offset, file_count);
 
+    /* for (int i = 0; i < file_count; i++) { */
+    /*     Entry e = entries[i]; */
+    /*     if (e.type == ENTRY_FILE) { */
+    /*         printf("  FILE %s\n", &info_data[e.name_offset]); */
+    /*     } else if (e.type == ENTRY_DIR) { */
+    /*         printf("  DIR %s\n", &info_data[e.name_offset]); */
+    /*     } */
+    /* } */
+    /* fflush(0); */
+
     i = 0;
     int stack[16] = {0};
     process_entry(info_data, file_data, entries, &i, stack, 0);
+}
+
+int main(int argc, char **argv) {
+
+    int ls_len;
+    Pack *pack_list = get_pack_list("/home/paul/Downloads/li2/cabs/Game/_data", &ls_len);
+    for (int i = 0; i < ls_len; i++) {
+        printf("%s\n", pack_list[i].bod);
+        process_pack(pack_list[i]);
+    }
 
     return 0;
 }
